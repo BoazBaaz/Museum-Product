111bdb585adfeb4cc24c586436501b91f971ad87
Boas-Bas van der Veen <57945427+BoazBaaz@users.noreply.github.com>
2023-03-24 14:47:04 +0100

    Player input added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,16 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    DefaultInputActions input;  
     
-
     // Start is called before the first frame update
     void Start() {
-        
+        input = new DefaultInputActions();
+        input.Enable();
     }
 
     // Update is called once per frame
     void Update() {
-        
+        Debug.Log(input.Player.Move.ReadValue<Vector2>());
     }
 }
