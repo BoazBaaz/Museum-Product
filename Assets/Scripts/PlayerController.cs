8b3d22357eaf29134fbc44cab0896539b1c0e685
Boas-Bas van der Veen <57945427+BoazBaaz@users.noreply.github.com>
2023-03-24 15:56:18 +0100

    Player movement intergraded

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,16 +5,26 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    DefaultInputActions input;  
+    DefaultInputActions input;
+    Rigidbody rb;
+
+    [SerializeField] float m_Speed = 10f;
     
     // Start is called before the first frame update
     void Start() {
         input = new DefaultInputActions();
         input.Enable();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update() {
-        Debug.Log(input.Player.Move.ReadValue<Vector2>());
+
+    }
+
+    void FixedUpdate() {
+        rb.MovePosition(new Vector3(gameObject.transform.position.x + input.Player.Move.ReadValue<Vector2>().x * m_Speed * Time.deltaTime,
+                                    gameObject.transform.position.y,
+                                    gameObject.transform.position.z + input.Player.Move.ReadValue<Vector2>().y * m_Speed * Time.deltaTime));
     }
 }
