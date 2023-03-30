810409be28c6ed5a9fe69c8178b5f2543ca63ac4
Boas-Bas van der Veen <Boas.Bas@Gmail.com>
2023-03-30 23:07:18 +0200

    Fuck c#
    I forgot how  to code c# ;_;

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,10 @@ public class PlayerController : MonoBehaviour
     DefaultInputActions input;
     Rigidbody rb;
 
-    [SerializeField] float m_Speed = 10f;
+    [SerializeField] float m_MoveSpeed = 10f;
+    [SerializeField] Vector2 m_MouseSensitivity = new Vector2(10.0f, 10.0f);
+    [SerializeField] Vector2 m_MouseClampY = new Vector2(-80.0f, 50.0f);
+    [SerializeField] [Range(0, 4)] int m_Keys = 0;
     
     // Start is called before the first frame update
     void Start() {
@@ -19,12 +22,30 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
-
     }
 
     void FixedUpdate() {
-        rb.MovePosition(new Vector3(gameObject.transform.position.x + input.Player.Move.ReadValue<Vector2>().x * m_Speed * Time.deltaTime,
-                                    gameObject.transform.position.y,
-                                    gameObject.transform.position.z + input.Player.Move.ReadValue<Vector2>().y * m_Speed * Time.deltaTime));
+        // Get the current position and rotation of the player and camera
+        Vector3 playerPosition = gameObject.transform.position;
+        Vector3 playerRotation = gameObject.transform.localEulerAngles;
+        Vector3 camRotation = Camera.main.transform.localEulerAngles;
+
+        // Calculate the new  player position
+        playerPosition.x += input.Player.Move.ReadValue<Vector2>().x * m_MoveSpeed * Time.deltaTime;
+        playerPosition.z += input.Player.Move.ReadValue<Vector2>().y * m_MoveSpeed * Time.deltaTime;
+
+        // Calculate the new player and camera rotation
+        playerRotation.y += input.Player.Look.ReadValue<Vector2>().x * m_MouseSensitivity.x * Time.deltaTime;
+        camRotation.x -= input.Player.Look.ReadValue<Vector2>().y * m_MouseSensitivity.y * Time.deltaTime;
+        //camRotation.x = Mathf.Clamp(camRotation.x, m_MouseClampY[0], m_MouseClampY[1]);
+
+        // Update the position and rotation of the player and the camera
+        rb.MovePosition(playerPosition);
+        rb.MoveRotation(Quaternion.Euler(playerRotation));
+        Camera.main.transform.localRotation = Quaternion.Euler(camRotation);
+    }
+
+    public void AddKey() {
+        m_Keys++;
     }
 }
